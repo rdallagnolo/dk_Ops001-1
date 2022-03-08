22958febb8f5a2a1e0ede5bc814e497bc4a71c79
Rodrigo Dallagnolo <rdallagnolo@gmail.com>
2022-03-08 16:51:49 -0300

    changes

--- Ops001-1_app.py
+++ Ops001-1_app.py
@@ -107,7 +107,7 @@ fig2.update_layout(barmode='stack',height=800, width=500,
                   paper_bgcolor='rgb(38, 39, 48)',
                   plot_bgcolor='rgb(38, 39, 48)',
                   font_color='#c7d0d8',
-                  margin=dict(l=20, r=20, t=20, b=20),
+                  margin=dict(l=20, r=20, t=20, b=20, pad=20),
                   xaxis = dict(tickfont = dict(size=15),tickangle=45),
                   yaxis = dict(tickfont = dict(size=15)))
 
